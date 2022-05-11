c83b5f93f0d71df9cc61de01dbe0037fc8b41d33
KevenMarioN <kevenmario.n.r@gmail.com>
2022-05-11 09:17:28 -0300

    feat: :sparkles: Create dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 function Dashboard(): JSX.Element {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/51131707?v=4" alt="Keven" />
+          <div>
+            <strong>KevenMarioN/foundation_golang</strong>
+            <p>Sem descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/51131707?v=4" alt="Keven" />
+          <div>
+            <strong>KevenMarioN/foundation_golang</strong>
+            <p>Sem descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/51131707?v=4" alt="Keven" />
+          <div>
+            <strong>KevenMarioN/foundation_golang</strong>
+            <p>Sem descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 }
 
 export default Dashboard;
